c6b68df00e0c279dc0280dbd2eddc15cf573b984
TRIFORCE89 <2404662+TRIFORCE89@users.noreply.github.com>
2018-07-02 11:55:02 -0400

    Fix for Substitute glitches

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -5280,8 +5280,15 @@ AttackSubstitute:
 	jr z,.nullifyEffect
 	ld hl,wEnemyMoveEffect ; value for enemy's turn
 .nullifyEffect
+	ld a, [hl] ; some effects don't need to be removed
+	cp HYPER_BEAM_EFFECT
+	jr z, .done
+	cp EXPLODE_EFFECT
+	jr z, .done
+	; if it wasn't one of those, nullify the effect
 	xor a
 	ld [hl],a ; zero the effect of the attacker's move
+.done
 	jp DrawHUDsAndHPBars
 
 SubstituteTookDamageText:
--- engine/battle/moveEffects/substitute_effect.asm
+++ engine/battle/moveEffects/substitute_effect.asm
@@ -37,8 +37,11 @@ SubstituteEffect_:
 	sbc 0
 	pop bc
 	jr c, .notEnoughHP ; underflow means user would be left with negative health
-                           ; bug: since it only brances on carry, it will possibly leave user with 0 HP
 .userHasZeroOrMoreHP
+	ld e, a
+	or d
+	ld a, e
+	jr z, .notEnoughHP ; fail if move would leave user with 0 hp
 	ldi [hl], a ; save resulting HP after substraction into current HP
 	ld [hl], d
 	ld h, b
